4773832f40bd52f3a1732abf81594af3cdbcf009
sestaton <statonse@gmail.com>
2013-07-18 09:16:31 -0400

    simplify options to run only mgblast

--- lib/Transposome/Blast.pm
+++ lib/Transposome/Blast.pm
@@ -2,24 +2,27 @@ package Blast;
 
 use 5.012;
 use Moose;
-use File::Basename;  # Moose equivalent, oh yeah, my file type
+#use File::Basename;  # Moose equivalent, oh yeah, my file type
 use namespace::autoclean;
 use SeqIO;
-with 'File', 'Types';
+with 'Types', 'File';
 
-# class attributes
 =head1 SYNOPSIS
 
     use Transposome::Blast;
 
-    my $seq_store = Transposome::Blast->new( file      => 'myseqs.fas',
-                                             in_memory => 1,
-                                               );
+    my $blast_obj = Transposome::Blast->new( file       => 'myseqs.fas',
+                                             seq_num    => 1_000_000,
+                                             aln_num    => 250,
+                                             desc_num   => 500, 
+                                             cpus       => 2, 
+                                             threads    => 12,
+                                            );
 
 =head1 ATTRIBUTES
 
-'seq_file'  - Takes the name of a Fasta/q file as the argument.
-'in_memory' - Set whether or not perform calculations in memory (Boolean: 0/1; Default: 0).
+'file'  - Takes the name of a Fasta/q file as the argument.
+
 
 =cut
 
@@ -41,24 +44,6 @@ has desc_num => (
     default => 500,
     );
 
-has blast_prog => (
-    is      => 'ro',
-    isa     => 'Str',
-    default => 'blastp',
-    );
-
-has blast_format => (
-    is      => 'ro',
-    isa     => 'Int',
-    default => 8,
-    );
-
-has e_value => (
-    is      => 'ro',
-    isa     => 'Any',
-    default => '1e-5',
-    );
-
 has cpus => (
     is      => 'ro',
     is      => 'Int',
@@ -76,20 +61,6 @@ has seq_num => (
     isa  => 'Int',
     );
 
-has seq_format => (
-    is         => 'ro',
-    isa        => 'Str',
-    lazy       => 1,
-    default    => 0,
-    );
-
-has seq_format => (
-    is         => 'ro',
-    isa        => 'Str',
-    lazy       => 1,
-    default    => 0,
-    );
-
 =head1 METHODS
 
 =cut
@@ -100,19 +71,14 @@ sub run_blast {
         $evalue, $warn) = @_;
 
     ## use file routines from MooseX::Types::Path::Class
-    my ($dbfile,$dbdir,$dbext) = fileparse($database, qr/\.[^.]*/);
-    my ($subfile,$subdir,$subext) = fileparse($subseq_file, qr/\.[^.]*/);
+    #my ($dbfile,$dbdir,$dbext) = fileparse($database, qr/\.[^.]*/);
+    #my ($subfile,$subdir,$subext) = fileparse($subseq_file, qr/\.[^.]*/);
+    my $dbfile = $database->file->basename;
+    my $dbdir  = $database->file->dir;
+    my $subfile = $subseq_file->file->basename;
+    my $subdir = $subseq_file->file->dir;
 
-    my $suffix;
-    if ($blast_format == 8) {
-        $suffix = ".bln";
-    }
-    elsif ($blast_format == 7) {
-        $suffix = ".blastxml";
-    }
-    elsif ($blast_format == 0) {
-        $suffix = ".$blast_program";
-    }
+    my $suffix = ".bln";
     my $subseq_out = $subfile."_".$dbfile.$suffix;
 
     my ($niceload, $blast_cmd, $exit_value);
@@ -149,8 +115,10 @@ sub run_blast {
 sub split_reads {
     my ($self, $infile, $outfile, $numseqs) = @_;
 
-    my ($iname, $ipath, $isuffix) = fileparse($infile, qr/\.[^.]*/);
-    
+    #my ($iname, $ipath, $isuffix) = fileparse($infile, qr/\.[^.]*/);
+    my $iname = $infile->file->basename;
+    my $ipath = $infile->file->dir;
+
     my $out;
     my $count = 0;
     my $fcount = 1;
@@ -171,29 +139,29 @@ sub split_reads {
     open my $in, '<', $infile or die "\nERROR: Could not open file: $infile\n";
     my @aux = undef;
 
-    if (-e $self->file) {
-        my $filename = $self->file->basename;
-        my $seqio = SeqIO->new( file => $filename );
-        my $fh = $seqio->get_fh;
-        while (my $seq = $seqio->next_seq($fh)) {
-
-	    if ($count % $numseqs == 0 && $count > 0) {
-		$fcount++;
-		$tmpiname = $iname."_".$fcount."_XXXX";
-		my $fname = File::Temp->new( TEMPLATE => $tmpiname,
-					     DIR      => $cwd,
-					     SUFFIX   => ".fasta",
-					     UNLINK   => 0);
-		
-		open $out, '>', $fname or die "\nERROR: Could not open file: $fname\n";
-		
-		push @split_files, $fname;
-	    }
-	    #say $out join "\n", ">".$name, $seq;
-	    say $out join "\n", ">".$seq->id, $seq->seq;
-	    $count++;
+    #if (-e $self->file) {
+    my $filename = $self->file->basename;
+    my $seqio = SeqIO->new( file => $filename );
+    my $fh = $seqio->get_fh;
+    while (my $seq = $seqio->next_seq($fh)) {
+	
+	if ($count % $numseqs == 0 && $count > 0) {
+	    $fcount++;
+	    $tmpiname = $iname."_".$fcount."_XXXX";
+	    my $fname = File::Temp->new( TEMPLATE => $tmpiname,
+					 DIR      => $cwd,
+					 SUFFIX   => ".fasta",
+					 UNLINK   => 0);
+	    
+	    open $out, '>', $fname or die "\nERROR: Could not open file: $fname\n";
+	    
+	    push @split_files, $fname;
 	}
+	#say $out join "\n", ">".$name, $seq;
+	say $out join "\n", ">".$seq->id, $seq->seq;
+	$count++;
     }
+    #}
     close $in; close $out;
     return (\@split_files, $count);
 }
