34baf118a1c43956ad17e6eb9fc78fce10a91973
sestaton <statonse@gmail.com>
2013-07-25 10:15:23 -0400

    updated SeqIO.pm with better exception handling; removing old SeqIO_n.pm

--- lib/Transposome/SeqIO.pm
+++ lib/Transposome/SeqIO.pm
@@ -1,7 +1,9 @@
-package SeqIO;
+package SeqIO_n;
 
 use 5.012;
 use Moose;
+use Try::Tiny;
+use Carp;
 use namespace::autoclean;
 
 with 'File';
@@ -44,37 +46,93 @@ sub next_seq {
     my $hline = <$fh>;
     return unless defined $hline;
     chomp $hline;
-    if (substr($hline, 0, 1) eq '>' || substr($hline, 0, 1) eq '@') {
-	if ($hline =~ /^.?(\S+)\s(\d)\S+/) {      # Illumina 1.8+
-	    $name = $1."/".$2;
-	    $self->set_id($name);
-	}
-	elsif ($hline =~ /^.?(\S+)/) {            # Illumina 1.3+
-	    $name = $1;
-	    $self->set_id($name);
-	} else {
-	    $name = '';                           # ?
-	    $self->set_id($name);
-	}
-	#say $self->id;
+    try {
+	die unless (substr($hline, 0, 1) eq '>' || substr($hline, 0, 1) eq '@');
     }
-    my $sline = <$fh>;
-    return unless defined $sline;
-    chomp $sline;
-    my $c = substr($sline, 0, 1);
-    last if ($c eq '>' || $c eq '@' || $c eq '+');
-    $seq .= $sline;
-    $self->set_seq($seq);
-    #say $self->seq;
-    return $self if ($c ne '+');
-    
-    my $qline = <$fh>;
-    chomp $qline;
-    return unless defined $qline;
-    $qual .= $qline;
-    if (length($qual) >= length($seq)) {
+    catch {
+	say "\nERROR: '$hline' does not look like Fasta or Fastq.\nHere is the exception: $_\n";
+	exit(1);
+    };
+    if (substr($hline, 0, 1) eq '>') {
+	my $name = _set_id_per_encoding($hline);
+	$self->set_id($name);
+
+	my $sline = <$fh>;
+	return unless defined $sline;
+	chomp $sline;
+	try {
+	    die if !length($sline);
+	}
+	catch {
+	    say "\nERROR: No sequence for Fasta record '$name'.\nHere is the exception: $_\n";
+	    exit(1);
+	};
+	$seq .= $sline;
+        $self->set_seq($seq);
+
+	return $self;
+    }
+    elsif (substr($hline, 0, 1) eq '@') {
+	my $name = _set_id_per_encoding($hline);
+	$self->set_id($name);
+
+        my $sline = <$fh>;
+        return unless defined $sline;
+        chomp $sline;
+	try {
+	    die if !length($sline);
+	}
+	catch {
+	    say "\nERROR: No sequence for Fastq record '$name'.\nHere is the exception: $_\n";
+	    exit(1);
+	};
+	$seq .= $sline;
+        $self->set_seq($seq);
+
+	my $cline = <$fh>;
+	return unless defined $cline;
+	chomp $cline;
+	try {
+	    die unless length($cline) && substr($cline, 0, 1) eq '+';
+	}
+	catch {
+	    say "\nERROR: No comment line for Fastq record '$name'.\nHere is the exception: $_\n";
+	    exit(1);
+	};
+
+	my $qline = <$fh>;
+	return unless defined $qline;
+	chomp $qline;
+	try {
+	    die if !length($qline);
+	}
+	catch {
+	    say "\nERROR: No quality scores for '$name'.\nHere is the exception: $_\n"; 
+	    exit(1);
+	};
+	$qual .= $qline;
+	try {
+	    die unless length($qual) >= length($seq);
+	}
+	catch {
+	    say "\nERROR: Unequal number of quality and scores and bases for '$name'.\nHere is the exception: $_\n";
+	    exit(1);
+	};
 	$self->set_qual($qual);
 	return $self;
+    }
+}
+
+sub _set_id_per_encoding {
+    my $hline = shift;
+    if ($hline =~ /^.?(\S+)\s(\d)\S+/) {
+	return $1."/".$2;
+    }
+    elsif ($hline =~ /^.?(\S+)/) {
+	return $1;
+    }
+    else {
+	return '';
     }
 }
 
