eb7b2fab3e1f8dca4b4f4e6a5ebcdfc93ec5ced3
sestaton <statonse@gmail.com>
2013-07-26 13:45:15 -0400

    module to handle files and directories as objects with attributes

---
+++ lib/Transposome/Role/File.pm
@@ -0,0 +1,34 @@
+package File;
+
+use 5.012;
+use Moose::Role;
+use MooseX::Types::Path::Class;
+use namespace::autoclean;
+
+
+# class attributes
+has 'file' => (
+      is       => 'ro',
+      isa      => 'Path::Class::File',
+      #isa      => 'File', # coerced to Path::Class::File
+      required => 1,
+      coerce   => 1,
+    );
+
+has 'dir' => (
+      is       => 'ro',
+      isa      => 'Path::Class::Dir',
+      required => 1,
+      coerce   => 1,
+    );
+
+# class methods
+sub get_fh {
+    my $self = shift;
+    if (-e $self->file) {
+        my $fh = $self->file->openr;
+        return $fh
+    }
+}
+
+1;
