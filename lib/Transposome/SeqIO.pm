9f35f5c08eda6d7620971027c7ebe3f6d42819c9
sestaton <statonse@gmail.com>
2013-08-02 06:21:39 -0400

    update pod, change error handling to follow conventions used

--- lib/Transposome/SeqIO.pm
+++ lib/Transposome/SeqIO.pm
@@ -105,8 +105,7 @@ sub next_seq {
 	die unless (substr($hline, 0, 1) eq '>' || substr($hline, 0, 1) eq '@');
     }
     catch {
-	say "\nERROR: '$hline' does not look like Fasta or Fastq.\nHere is the exception: $_\n";
-	exit(1);
+	warn "\n[ERROR]: '$hline' does not look like Fasta or Fastq.\nHere is the exception: $_\n" and exit;
     };
     if (substr($hline, 0, 1) eq '>') {
 	my $name = _set_id_per_encoding($hline);
@@ -119,8 +118,7 @@ sub next_seq {
 	    die if !length($sline);
 	}
 	catch {
-	    say "\nERROR: No sequence for Fasta record '$name'.\nHere is the exception: $_\n";
-	    exit(1);
+	    warn "\n[ERROR]: No sequence for Fasta record '$name'.\nHere is the exception: $_\n" and exit;
 	};
 	$seq .= $sline;
         $self->set_seq($seq);
@@ -138,8 +136,7 @@ sub next_seq {
 	    die if !length($sline);
 	}
 	catch {
-	    say "\nERROR: No sequence for Fastq record '$name'.\nHere is the exception: $_\n";
-	    exit(1);
+	    warn "\n[ERROR]: No sequence for Fastq record '$name'.\nHere is the exception: $_\n" and exit;
 	};
 	$seq .= $sline;
         $self->set_seq($seq);
@@ -151,8 +148,7 @@ sub next_seq {
 	    die unless length($cline) && substr($cline, 0, 1) eq '+';
 	}
 	catch {
-	    say "\nERROR: No comment line for Fastq record '$name'.\nHere is the exception: $_\n";
-	    exit(1);
+	    warn "\n[ERROR]: No comment line for Fastq record '$name'.\nHere is the exception: $_\n" and exit;
 	};
 
 	my $qline = <$fh>;
@@ -162,16 +158,14 @@ sub next_seq {
 	    die if !length($qline);
 	}
 	catch {
-	    say "\nERROR: No quality scores for '$name'.\nHere is the exception: $_\n"; 
-	    exit(1);
+	    warn "\n[ERROR]: No quality scores for '$name'.\nHere is the exception: $_\n" and exit;
 	};
 	$qual .= $qline;
 	try {
 	    die unless length($qual) >= length($seq);
 	}
 	catch {
-	    say "\nERROR: Unequal number of quality and scores and bases for '$name'.\nHere is the exception: $_\n";
-	    exit(1);
+	    warn "\n[ERROR]: Unequal number of quality and scores and bases for '$name'.\nHere is the exception: $_\n" and exit;
 	};
 	$self->set_qual($qual);
 	return $self;
@@ -206,6 +200,55 @@ sub _set_id_per_encoding {
 	return '';
     }
 }
+
+=head1 AUTHOR
+
+S. Evan Staton, C<< <statonse at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests through the project site at 
+L<https://github.com/sestaton/Transposome/issues>. I will be notified,
+and there will be a record of the issue. Alternatively, I can also be 
+reached at the email address listed above to resolve any questions.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Transposome::SeqIO
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 S. Evan Staton.
+
+This program is distributed under the MIT (X11) License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+
+=cut
 
 __PACKAGE__->meta->make_immutable;
 
