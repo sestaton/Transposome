d8a080bf655fec66410f86a941a6d88b1782ff08
sestaton <statonse@gmail.com>
2013-08-02 06:22:30 -0400

    remove cpan boilerplate and add github issue tracker links

--- lib/Transposome/SeqStore.pm
+++ lib/Transposome/SeqStore.pm
@@ -76,7 +76,6 @@ sub store_seq {
 
     if (-e $self->file) {
 	my $filename = $self->file->relative;
-	#say $name and exit;
 	my $seqio = Transposome::SeqIO->new( file => $filename );
 	my $fh = $seqio->get_fh;
 	while (my $seq = $seqio->next_seq($fh)) {
@@ -93,41 +92,16 @@ S. Evan Staton, C<< <statonse at gmail.com> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-transposome at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Transposome>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests through the project site at 
+L<https://github.com/sestaton/Transposome/issues>. I will be notified,
+and there will be a record of the issue. Alternatively, I can also be 
+reached at the email address listed above to resolve any questions.
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc Transposome::SeqStore
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Transposome>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Transposome>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Transposome>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Transposome/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
 
 
 =head1 LICENSE AND COPYRIGHT
@@ -161,5 +135,4 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 =cut
 
-#1; # End of Transposome::SeqStore
 __PACKAGE__->meta->make_immutable;
