7bfa65b87c6f6fc74b1f34459600407df3a8edc7
sestaton <statonse@gmail.com>
2013-08-07 15:21:51 -0400

    add pod; remove the loading of Cwd

--- t/lib/TestUtils/TestConfig.pm
+++ t/lib/TestUtils/TestConfig.pm
@@ -4,9 +4,29 @@ use 5.012;
 use Moose::Role;
 use MooseX::Types::Path::Class;
 use File::Temp;
-use Cwd;
 use namespace::autoclean;
 use TestUtils;
+
+=head1 NAME
+
+TestUtils::TestConfig - Methods for mocking configuration data for Transposome.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+    our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use TestUtils;
+
+    my $test = TestUtils->new( build_proper => 1, destroy => 0 );
+    my $config_arr = $test->config_constructor;
+
+=cut
 
 has 'seq_file' => (
     is       => 'ro',
@@ -22,6 +42,22 @@ has 'repeat_db' => (
     coerce   => 1,
     );
 
+=head2 config_constructor
+
+ Title   : config_constructor
+
+ Usage   : my $test = TestUtils->new( build_proper => 1, destroy => 0 );                                                                                                         
+           my $fa_arr = $test->config_constructor; 
+           
+ Function: Create temporary configuration files for testing Transposome.
+
+                                                                            Return_type
+ Returns : An array of config files.                                         ArrayRef
+           
+ Args    : None. This is a class method called on TestUtils object.
+
+=cut
+
 sub config_constructor {
     my ($self) = @_;
     if ($self->build_proper) {
@@ -35,9 +71,24 @@ sub config_constructor {
     }
 }
 
+=head2 _build_config_data
+
+ Title   : _build_config_data
+
+ Usage   : This is a private method, don't use it directly.
+           
+ Function: Create temporary configuration files for testing Transposome.
+
+                                                                            Return_type
+ Returns : A config file                                                    Scalar
+           
+ Args    : None. This is a class method called on TestUtils object.
+
+=cut
+
 sub _build_config_data {
     my ($self) = @_;
-    my $cwd = getcwd();
+
     my $tmpyml = File::Temp->new( TEMPLATE => "transposome_config_XXXX",
                                   DIR      => 't',
                                   SUFFIX   => ".yml",
@@ -72,5 +123,40 @@ sub _build_config_data {
 
     return $tmpyml_name;
 }
+
+=head1 AUTHOR
+
+S. Evan Staton, C<< <statonse at gmail.com> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 S. Evan Staton.
+
+This program is distributed under the MIT (X11) License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+
+=cut
 
 1;
