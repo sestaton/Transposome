77100e9013b18d1ec86882c3d77df19b303d8f95
sestaton <statonse@gmail.com>
2013-08-07 13:45:50 -0400

    fix pod formatting

--- lib/Transposome/Annotation/Typemap.pm
+++ lib/Transposome/Annotation/Typemap.pm
@@ -2,7 +2,6 @@ package Transposome::Annotation::Typemap;
 
 use 5.012;
 use Moose::Role;
-use Data::Dump qw(dd);
 use Storable qw(freeze);
 
 =head1 NAME
@@ -56,11 +55,11 @@ our $VERSION = '0.01';
  Function: Create a mapping of the input repeats to the full repeat 
            taxonomic lineage.
 
-                                                                            Data_type
+                                                                            Return_type
  Returns : A complex data structure of the repeat taxonomy from Class       HashRef
            to the element belonging to a particular family
             
-                                                                            Data_type
+                                                                            Arg_type
  Args    : The repeat database being used for annotation                    Scalar
 
 =cut
@@ -128,8 +127,6 @@ sub map_repeat_types {
 	}
     }
     
-    #dd $matches;
-
     my $matches_ser = freeze $matches;   
     return $matches_ser;
 }
@@ -142,8 +139,8 @@ sub map_repeat_types {
            
  Function: Creates a data structure containing a full taxonomic
            map of repeat types.
-                                                                   Return_type
- Returns : A complex data structure containing a mapping of        HashRef
+                                                                            Return_type
+ Returns : A complex data structure containing a mapping of                 HashRef
            repeat types from Class to Family
 
  Args    : None. This is a class method called on a object
@@ -201,5 +198,54 @@ sub _build_repeat_map {
     
     return $matches;
 }
+
+=head1 AUTHOR
+
+S. Evan Staton, C<< <statonse at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests through the project site at 
+L<https://github.com/sestaton/Transposome/issues>. I will be notified,
+and there will be a record of the issue. Alternatively, I can also be 
+reached at the email address listed above to resolve any questions.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Transposome::Annotation::Typemap
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 S. Evan Staton.
+
+This program is distributed under the MIT (X11) License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+
+=cut
 
 1;
