0fb9855d38d425a337329b222349366d87508f5b
sestaton <statonse@gmail.com>
2013-09-05 17:21:33 -0400

    rollback on exception handling method to clean qual string

--- lib/Transposome/SeqIO.pm
+++ lib/Transposome/SeqIO.pm
@@ -182,13 +182,7 @@ sub next_seq {
         }
         seek $fh, -length($qline)-1, 1 if length $qline;
         my $qual = join '', @quals;
-	try {
-	    die if $qual !~ /${pat}/;
-	}
-	catch {
-	    warn "\n[ERROR]: Different IDs found for record at line $. not matching '$name'.\nHere is the exception: $_\n" and exit(1);
-	};
-        $qual =~ s/${pat}.*//;# if $qual =~ /${pat}/;
+        $qual =~ s/${pat}.*// if $qual =~ /${pat}/;
 	$qual =~ s/\s//g;
 	try {
 	    die if !length($qual);
