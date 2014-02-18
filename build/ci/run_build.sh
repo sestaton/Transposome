b4886543de9dd38af1940a09bfa36d1a9f70e4f7
sestaton <statonse@gmail.com>
2014-02-18 16:42:29 -0500

    make sure where are in the top directory

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -2,7 +2,9 @@
 
 wget sourceforge.net/projects/gicl/files/mgblast.tar.gz
 cd mgblast
-make && make install
+make
+sudo make install
+cd ..
 
 sudo apt-get install libdb-dev libdb++-dev ncbi-blast+
 
