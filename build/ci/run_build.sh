79708aed3723f73fc9d380e6e610361746a0a878
sestaton <statonse@gmail.com>
2014-02-19 10:26:13 -0500

    modify install path for deps

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -3,15 +3,15 @@
 ## Fetch mgblast and modify Makefile for location of NCBI Toolkit
 wget sourceforge.net/projects/gicl/files/mgblast.tar.gz && tar xzf mgblast.tar.gz
 cd mgblast
-dir=`pwd`
-sed "s,NCBIDIR = .*,NCBIDIR = $dir/ncbi," Makefile > Makefile.bak
-mv Makefile.bak Makefile
+#dir=`pwd`
+#sed "s,NCBIDIR = .*,NCBIDIR = $dir/ncbi," Makefile > Makefile.bak
+#mv Makefile.bak Makefile
 
 ## Fetch and compile NCBI Toolkit, including deps
 sudo apt-get -y install csh xorg-dev openbox lesstif2-dev
 wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/old/20060507/ncbi.tar.gz && tar xzf ncbi.tar.gz
-sed 's/gcc -ansi"/gcc -ansi -Wno-deprecated"/' ncbi/make/makeall.unx > ncbi/make/makeall.unx.bak
-mv ncbi/make/makeall.unx.bak ncbi/make/makeall.unx
+#sed 's/gcc -ansi"/gcc -ansi -Wno-deprecated"/' ncbi/make/makeall.unx > ncbi/make/makeall.unx.bak
+#mv ncbi/make/makeall.unx.bak ncbi/make/makeall.unx
 
 ## This needs to be run three times: http://www.cslu.ogi.edu/~zak/debianclusters/Installing_mpiBLAST
 ./ncbi/make/makedis.csh 2>&1 > /dev/null
@@ -20,7 +20,8 @@ mv ncbi/make/makeall.unx.bak ncbi/make/makeall.unx
 # What kind of sorcery is this?
 
 ## Compile mgblast
-make CFLAGS="-Wno-deprecated"
+make
+cd ..
 
 ## Fetch legacy blast executables so we have formatdb
 wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/release/2.2.14/blast-2.2.14-x64-linux.tar.gz
