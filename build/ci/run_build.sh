9831b35d0995681339755ff6263ef6712ef160c2
sestaton <statonse@gmail.com>
2014-02-19 08:58:50 -0500

    suppress compiler warnings during tests

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -10,6 +10,8 @@ mv Makefile.bak Makefile
 ## Fetch and compile NCBI Toolkit, including deps
 sudo apt-get -y install csh xorg-dev openbox lesstif2-dev
 wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/old/20060507/ncbi.tar.gz && tar xzf ncbi.tar.gz
+sed 's/gcc -ansi"/gcc -ansi -Wno-deprecated"/' ncbi/make/makeall.unx > ncbi/make/makeall.unx.bak
+mv ncbi/make/makeall.unx.bak ncbi/make/makeall.unx
 
 ## This needs to be run three times: http://www.cslu.ogi.edu/~zak/debianclusters/Installing_mpiBLAST
 ./ncbi/make/makedis.csh 2>&1 > /dev/null
@@ -18,7 +20,7 @@ wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/old/20060507/ncbi.tar.gz && tar x
 # What kind of sorcery is this?
 
 ## Compile mgblast
-make
+make CFLAGS="-Wno-deprecated"
 
 ## Fetch legacy blast executables so we have formatdb
 wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/release/2.2.14/blast-2.2.14-x64-linux.tar.gz
