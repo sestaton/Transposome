2dac082cfc4c9df5b120de6d1e4415a4ec67ee70
sestaton <statonse@gmail.com>
2014-02-19 10:48:23 -0500

    clean up build script

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-## Fetch mgblast and modify Makefile for location of NCBI Toolkit
+## Fetch mgblast and modify makefile for location of NCBI Toolkit
 wget sourceforge.net/projects/gicl/files/mgblast.tar.gz && tar xzf mgblast.tar.gz
 cd mgblast
-#dir=`pwd`
-#sed "s,NCBIDIR = .*,NCBIDIR = $dir/ncbi," Makefile > Makefile.bak
-#mv Makefile.bak Makefile
+dir=`pwd`
+sed "s,NCBIDIR = .*,NCBIDIR = $dir/ncbi," Makefile > Makefile.bak
+mv Makefile.bak Makefile
 
 ## Fetch and compile NCBI Toolkit, including deps
 sudo apt-get -y install csh xorg-dev openbox lesstif2-dev
 wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/old/20060507/ncbi.tar.gz && tar xzf ncbi.tar.gz
-#sed 's/gcc -ansi"/gcc -ansi -Wno-deprecated"/' ncbi/make/makeall.unx > ncbi/make/makeall.unx.bak
-#mv ncbi/make/makeall.unx.bak ncbi/make/makeall.unx
 
 ## This needs to be run three times: http://www.cslu.ogi.edu/~zak/debianclusters/Installing_mpiBLAST
 ./ncbi/make/makedis.csh 2>&1 > /dev/null
@@ -27,11 +25,6 @@ cd ..
 wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/release/2.2.14/blast-2.2.14-x64-linux.tar.gz
 tar xzf blast-2.2.14-x64-linux.tar.gz
 
-## Export PATH to formatdb
-#export PATH=$PATH:blast-2.2.14/bin
-
-## install blast+ and bdb
+## install blast+ and BerkelyDB
 sudo apt-get -y install libdb-dev libdb++-dev ncbi-blast+
 
-## Export PATH to mgblast
-#export PATH=$PATH:$dir
