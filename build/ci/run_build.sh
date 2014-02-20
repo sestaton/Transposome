927f173d90d26dc0e2dc8778b230711940a6b930
sestaton <statonse@gmail.com>
2014-02-19 19:11:13 -0500

    remove extra package install; quiet down installation

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -8,23 +8,23 @@ sed "s,NCBIDIR = .*,NCBIDIR = $dir/ncbi," Makefile > Makefile.bak
 mv Makefile.bak Makefile
 
 ## Fetch and compile NCBI Toolkit, including deps
-sudo apt-get -y install csh xorg-dev openbox lesstif2-dev
+sudo apt-get -qq -y install csh xorg-dev openbox lesstif2-dev
 wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/old/20060507/ncbi.tar.gz && tar xzf ncbi.tar.gz
 
 ## This needs to be run three times: http://www.cslu.ogi.edu/~zak/debianclusters/Installing_mpiBLAST
 ./ncbi/make/makedis.csh 2>&1 > /dev/null
-./ncbi/make/makedis.csh 2>&1 > /dev/null
-./ncbi/make/makedis.csh 2>&1 > /dev/null
+#./ncbi/make/makedis.csh 2>&1 > /dev/null
+#./ncbi/make/makedis.csh 2>&1 > /dev/null
 # What kind of sorcery is this?
 
 ## Compile mgblast
 make
-cd ..
+#cd ..
 
 ## Fetch legacy BLAST executables so we have formatdb
-wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/release/2.2.14/blast-2.2.14-x64-linux.tar.gz
-tar xzf blast-2.2.14-x64-linux.tar.gz
+#wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/release/2.2.14/blast-2.2.14-x64-linux.tar.gz
+#tar xzf blast-2.2.14-x64-linux.tar.gz
 
 ## Install BLAST+ and BerkeleyDB
-sudo apt-get -y install libdb-dev libdb++-dev ncbi-blast+
+sudo apt-get -qq -y install libdb-dev libdb++-dev ncbi-blast+
 
