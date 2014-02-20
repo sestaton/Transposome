afed5f63169a42db951ff175bd9913825de975a8
sestaton <statonse@gmail.com>
2014-02-19 19:17:28 -0500

    clean up and simplify build process

--- build/ci/run_build.sh
+++ build/ci/run_build.sh
@@ -11,19 +11,11 @@ mv Makefile.bak Makefile
 sudo apt-get -qq -y install csh xorg-dev openbox lesstif2-dev
 wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/old/20060507/ncbi.tar.gz && tar xzf ncbi.tar.gz
 
-## This needs to be run three times: http://www.cslu.ogi.edu/~zak/debianclusters/Installing_mpiBLAST
+## Compile the NCBI Toolkit
 ./ncbi/make/makedis.csh 2>&1 > /dev/null
-#./ncbi/make/makedis.csh 2>&1 > /dev/null
-#./ncbi/make/makedis.csh 2>&1 > /dev/null
-# What kind of sorcery is this?
 
 ## Compile mgblast
 make
-#cd ..
-
-## Fetch legacy BLAST executables so we have formatdb
-#wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/release/2.2.14/blast-2.2.14-x64-linux.tar.gz
-#tar xzf blast-2.2.14-x64-linux.tar.gz
 
 ## Install BLAST+ and BerkeleyDB
 sudo apt-get -qq -y install libdb-dev libdb++-dev ncbi-blast+
