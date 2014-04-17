126f310917290ffd7c7171fa26f55d23e3af2401
sestaton <statonse@gmail.com>
2014-04-17 12:01:43 -0400

    separate tests from build

---
+++ build/ci/run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PATH=$PATH:/home/travis/build/sestaton/Transposome/mgblast
+export PATH=$PATH:/home/travis/build/sestaton/Transposome/mgblast/ncbi/bin
+
+perl Makefile.PL
+make
+make test
