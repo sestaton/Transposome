9a76e746409d8ad0aa7fdfb1da160af666102663
sestaton <statonse@gmail.com>
2014-09-23 14:58:28 -0400

    add import for debian build

--- src/main_community.cpp
+++ src/main_community.cpp
@@ -14,6 +14,7 @@
 //-----------------------------------------------------------------------------
 // see readme.txt for more details
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string>
