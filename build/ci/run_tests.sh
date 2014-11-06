01baa6221cd25467cc90b20b648a1234af9f8007
sestaton <statonse@gmail.com>
2014-11-05 22:05:05 -0500

    remove path modifications for previous build method

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#export PATH=$PATH:`pwd`/mgblast
-#export PATH=$PATH:`pwd`/mgblast/ncbi/bin
 export PATH=$PATH:/home/travis/build/sestaton/Transposome/build/ci/bin
 
 perl Makefile.PL && make && make test
