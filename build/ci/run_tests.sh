015e7daf7914de38b0136e5cb4e3648325e70fa6
sestaton <statonse@gmail.com>
2014-11-21 13:44:48 -0500

    check required libs

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -5,10 +5,10 @@ export PATH=$PATH:/home/travis/build/sestaton/Transposome/build/ci/bin
 echo "PATH: "
 ls -la /home/travis/build/sestaton/Transposome/build/ci/bin
 
-echo "FULL PATH TO EXE: "
-./home/travis/build/sestaton/Transposome/build/ci/bin/mgblast
+echo "LIBS LINKED: "
+ldd /home/travis/build/sestaton/Transposome/build/ci/bin/mgblast
 
-echo "ATTEMPT TO EX CMD: "
-mgblast
+#echo "ATTEMPT TO EX CMD: "
+#mgblast
 
 perl Makefile.PL && make && make test
