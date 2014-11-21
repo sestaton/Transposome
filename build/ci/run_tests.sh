03558cca6727f0dd42b685e816be7ca9293401cd
sestaton <statonse@gmail.com>
2014-11-21 13:18:06 -0500

    fix path for build

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -2,6 +2,6 @@
 
 export PATH=$PATH:/home/travis/build/sestaton/Transposome/build/ci/bin
 
-ls -la /home/travis/build/sestaton/Transposome/ci/bin
+ls -la /home/travis/build/sestaton/Transposome/build/ci/bin
 
 perl Makefile.PL && make && make test
