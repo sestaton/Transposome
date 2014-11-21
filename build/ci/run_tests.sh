849539efc095973eb1ef2c0807413475358910aa
sestaton <statonse@gmail.com>
2014-11-21 17:54:14 -0500

    clean up after debugging

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -2,11 +2,11 @@
 
 export PATH=$PATH:/home/travis/build/sestaton/Transposome/build/ci/bin
 
-echo "PATH: "
-ls -la /home/travis/build/sestaton/Transposome/build/ci/bin
+#echo "PATH: "
+#ls -la /home/travis/build/sestaton/Transposome/build/ci/bin
 
-echo "LIBS LINKED: "
-ldd /home/travis/build/sestaton/Transposome/build/ci/bin/mgblast
+#echo "LIBS LINKED: "
+#ldd /home/travis/build/sestaton/Transposome/build/ci/bin/mgblast
 
 #echo "ATTEMPT TO EX CMD: "
 #mgblast
