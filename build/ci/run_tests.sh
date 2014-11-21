4b813fce980c4a8b93ffec8f86ea644e38ee8fb2
sestaton <statonse@gmail.com>
2014-11-21 13:27:46 -0500

    add some debugging to resolve paths

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -2,6 +2,13 @@
 
 export PATH=$PATH:/home/travis/build/sestaton/Transposome/build/ci/bin
 
+echo "PATH: "
 ls -la /home/travis/build/sestaton/Transposome/build/ci/bin
+
+echo "FULL PATH TO EXE: "
+./home/travis/build/sestaton/Transposome/build/ci/bin/mgblast
+
+echo "ATTEMPT TO EX CMD: "
+mgblast
 
 perl Makefile.PL && make && make test
