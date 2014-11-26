043c26f4b87ac156e1afe5ebd1a624e0eeb241cd
sestaton <statonse@gmail.com>
2014-11-25 20:04:42 -0500

    clean up from debugging

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -2,13 +2,4 @@
 
 export PATH=$PATH:/home/travis/build/sestaton/Transposome/bin
 
-#echo "PATH: "
-#ls -la /home/travis/build/sestaton/Transposome/build/ci/bin
-
-#echo "LIBS LINKED: "
-#ldd /home/travis/build/sestaton/Transposome/build/ci/bin/mgblast
-
-#echo "ATTEMPT TO EX CMD: "
-#mgblast
-
 perl Makefile.PL && make && make test
