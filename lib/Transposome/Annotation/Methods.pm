74640df202eaec52a9188d5737634c55614eab23
sestaton <statonse@gmail.com>
2015-03-16 21:01:42 -0400

    add interface role for annotation methods

---
+++ lib/Transposome/Annotation/Methods.pm
@@ -0,0 +1,75 @@
+package Transposome::Annotation::Methods;
+
+use 5.010;
+use Moose::Role;
+
+with 'Transposome::Annotation::Typemap', 
+     'Transposome::Annotation::Mapping',
+     'Transposome::Annotation::Search',
+     'Transposome::Annotation::Summary';
+
+=head1 NAME
+
+Transposome::Annotation::Methods - An interface role to simplify importing all annotation methods.
+
+=head1 VERSION
+
+Version 0.09.1
+
+=cut
+
+our $VERSION = '0.09.1';
+$VERSION = eval $VERSION;
+
+=head1 SYNOPSIS
+
+    Consume this role in your class, or use Transposome::Annotation directly. E.g.,
+
+    use Transposome::Annotation;
+
+    my $cluster_file = '/path/to/cluster_file.cls';
+    my $seqct        = 'total_seqs_in_analysis';    # Integer
+    my $cls_tot      = 'total_reads_clustered';     # Integer
+
+    my $annotation = Transposome::Annotation->new( database  => 'repeat_db.fas',
+                                                   dir       => 'outdir',
+                                                   file      => 'report.txt' );
+
+    my $annotation_results =
+        $annotation->annotate_clusters({ cluster_directory => $cls_dir_path,
+                                         singletons_file => $singletons_file_path,
+                                         total_sequence_num => $seqct,
+                                         total_cluster_num => $cls_tot });
+    
+    $annotation->clusters_annotation_to_summary( $annotation_results );
+
+=cut
+
+=head1 AUTHOR
+
+S. Evan Staton, C<< <statonse at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests through the project site at 
+L<https://github.com/sestaton/Transposome/issues>. I will be notified,
+and there will be a record of the issue. Alternatively, I can also be 
+reached at the email address listed above to resolve any questions.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Transposome::Annotation::Methods
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2013-2015 S. Evan Staton
+
+This program is distributed under the MIT (X11) License, which should be distributed with the package. 
+If not, it can be found here: L<http://www.opensource.org/licenses/mit-license.php>
+
+=cut
+
+1;
