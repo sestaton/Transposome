eb44cb2d04be0e585a53047957a6469faa9d3d88
sestaton <statonse@gmail.com>
2015-03-16 23:18:25 -0400

    build package

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -2,6 +2,6 @@
 
 #export PATH=$PATH:/home/travis/build/sestaton/Transposome/bin
 
-perl Makefile.PL && /home/travis/build/sestaton/Transposome/blib/lib && prove -lv t/11-annotation.t
+perl Makefile.PL && make && /home/travis/build/sestaton/Transposome/blib/lib && prove -lv t/11-annotation.t
 
 #make test
