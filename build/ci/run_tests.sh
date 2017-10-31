cea86b95af2911b6375d5a58bea48e930ff1270a
sestaton <statonse@gmail.com>
2017-10-31 15:29:52 -0700

    run cover for tests

--- build/ci/run_tests.sh
+++ build/ci/run_tests.sh
@@ -2,7 +2,6 @@
 
 #export PATH=$PATH:/home/travis/build/sestaton/Transposome/bin
 
-perl Makefile.PL && make 
-#&& cover -test -report coveralls
-prove -bv t/09-megablast.t
+perl Makefile.PL && make && cover -test -report coveralls
+
 #make test
