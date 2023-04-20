4a4a871ced54fb04921b6488b40e1b2236d17410
sestaton <statonse@gmail.com>
2023-04-20 16:23:25 -0500

    update copyright date

--- lib/Transposome/Analysis/Pipeline.pm
+++ lib/Transposome/Analysis/Pipeline.pm
@@ -443,7 +443,7 @@ You can find documentation for this module with the perldoc command.
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2013-2017 S. Evan Staton
+Copyright (C) 2013-2023 S. Evan Staton
 
 This program is distributed under the MIT (X11) License, which should be distributed with the package. 
 If not, it can be found here: L<http://www.opensource.org/licenses/mit-license.php>
